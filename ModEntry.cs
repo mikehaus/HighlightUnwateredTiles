60041085cd9f89e8fb260005dc67ad3cff48eb3b
mikehaus <mike.haus.dev@gmail.com>
2024-12-23 16:35:12 -0700

    First Commit

---
+++ ModEntry.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+
+namespace HighlightUnwateredTiles
+{
+    public class ModEntry
+    {
+    }
+}
