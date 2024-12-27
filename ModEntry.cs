554b681aac149c9a949a24cf23e9c6df286846d3
mikehaus <mike.haus.dev@gmail.com>
2024-12-27 13:42:43 -0700

    Update get waterable crop locations

--- ModEntry.cs
+++ ModEntry.cs
@@ -7,68 +7,92 @@ using StardewModdingAPI.Utilities;
 using StardewValley;
 using StardewValley.GameData.Crops;
 using StardewValley.SDKs;
+using StardewValley.TerrainFeatures;
 
 namespace HighlightUnwateredTiles
 {
     public class ModEntry : Mod
     {
         private bool isLaunched;
-        private List<string> cropsToHighlight = new List<string>();
+        private Dictionary<int, List<Vector2>> cropLocations = new Dictionary<int, List<Vector2>>();
         // private Stage CurrentStage = Stage.none;
-        
+
         public override void Entry(IModHelper helper)
         {
             helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
             helper.Events.GameLoop.DayStarted += this.OnDayStarted;
-            helper.Events.GameLoop.UpdateTicked += this.CheckForCropsToWater;
+            helper.Events.Player.Warped += this.HandleWarped;
         }
 
         private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
         {
-           // Handle Initial Lauched items here 
-           this.Monitor.Log("Loaded highlight unwatered crops");
-           this.isLaunched = true;
+            // Handle Initial Lauched items here 
+            this.Monitor.Log("Loaded highlight unwatered crops");
+            this.isLaunched = true;
         }
 
         private void OnDayStarted(object? sender, DayStartedEventArgs e)
         {
             if (!this.isLaunched)
                 return;
-            
-            this.Monitor.Log("READING CROP INFO");
-            var crops = Game1.cropData;
-            
-            foreach (var crop in crops)
-            {
-                this.Monitor.Log($"Crop ID: {crop.Key}, Data: {crop.Value}", LogLevel.Info);
-                if (crop.Value.NeedsWatering)
-                    cropsToHighlight.Add(crop.Key);
-            }
 
-            foreach (var cropId in cropsToHighlight)
-            {
-                this.Monitor.Log($"CROP NEEDS WATERING: {cropId}", LogLevel.Info);
-            }
+            this.Monitor.Log("READING CROP INFO");
         }
 
-        // private void HandleReadCrops(object? sender, EventArgs? e)
-        private void HandleReadCrops()
+        private void HandleWarped(object? sender, WarpedEventArgs e)
+        {
+            if (!Context.IsWorldReady) return;
+
+            this.Monitor.Log($"Entered new map location {e.NewLocation.Name}");
+
+            // TODO: enhance this with specific location logic
+            this.UpdateWaterableCropLocations();
+        }
+
+        private void UpdateWaterableCropLocations()
         {
             if (!Context.IsWorldReady)
                 return;
 
             var crops = Game1.cropData;
+            this.cropLocations.Clear();
 
             foreach (var crop in crops)
             {
                 this.Monitor.Log($"Crop ID: {crop.Key}, Data: {crop.Value}", LogLevel.Info);
             }
-        }
 
-        private void CheckForCropsToWater(object? sender, UpdateTickedEventArgs e)
-        {
-            
+            GameLocation farm = Game1.getLocationFromName("Farm");
+            foreach (var tile in farm.terrainFeatures.Pairs)
+            {
+                // if (tile.Value is not StardewValley.TerrainFeatures.HoeDirt) continue;
+                // HoeDirt is a location where crops are planted
+                if (tile.Value is StardewValley.TerrainFeatures.HoeDirt dirt)
+                {
+                    if (dirt.crop != null && dirt.needsWatering())
+                    {
+                        Vector2 pos = tile.Key;
+                        Crop crop = dirt.crop;
+
+                        this.Monitor.Log($"Crop: {crop.rowInSpriteSheet.Name} id: {crop.rowInSpriteSheet.Value}");
+                        int cropId = crop.rowInSpriteSheet.Value;
+                        if (!this.cropLocations.ContainsKey(cropId))
+                        {
+                            this.cropLocations[cropId] = new List<Vector2>();
+                        }
+
+                        this.cropLocations[cropId].Add(pos);
+                    }
+                }
+            }
+
+            foreach (var cropLocation in this.cropLocations)
+            {
+                foreach (var i in cropLocation.Value)
+                {
+                    this.Monitor.Log($"Crop ID: {cropLocation.Key}, xCoord: {i.X}, yCoord: {i.Y}");
+                }
+            }
         }
-        
     }
 }
